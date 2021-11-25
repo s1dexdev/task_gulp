2c63286947353a8063b1bd56b54cabdf573fee81
s1dexdev <maksym.se@gmail.com>
2021-11-25 18:25:03 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,89 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+'use strict';
+
+const { watch, series, parallel, src, dest } = require('gulp');
+
+const browsersync = require('browser-sync').create();
 const ts = require('gulp-typescript');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const concat = require('gulp-concat');
+const tsProject = ts.createProject('./tsconfig.json');
+
+const scss = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const del = require('del');
+
+const path = {
+    build: {
+        html: './build/',
+        js: './build/js/',
+        css: './build/css/',
+    },
+    src: {
+        html: './src/**/*.html',
+        ts: './src/ts/*.ts',
+        scss: './src/scss/*.scss',
+    },
+    watch: {
+        html: './src/**/*.html',
+        ts: './src/ts/*.ts',
+        scss: './src/scss/*.scss',
+    },
+    clean: './build',
+};
+let build = null;
+let observe = null;
+
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: './build/',
+        },
+        port: 3000,
+        notify: false,
+        open: false,
+    });
+}
+
+function html() {
+    return src(path.src.html)
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream());
+}
+
+function styles() {
+    return src(path.src.scss)
+        .pipe(
+            scss({
+                outputStyle: 'compressed',
+            }),
+        )
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream());
+}
+
+function typescript() {
+    return src(path.src.ts)
+        .pipe(tsProject())
+        .js.pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream());
+}
+
+function watchFiles() {
+    watch([path.watch.html], html);
+    watch([path.watch.scss], styles);
+    watch([path.watch.ts], typescript);
+}
+
+function clean() {
+    return del(path.clean);
+}
+
+build = series(clean, html, styles, typescript);
+observe = parallel(build, watchFiles, browserSync);
+
+exports.build = build;
+exports.observe = observe;
+
+exports.default = observe;
--- src/ts/restaurant.ts
+++ src/ts/restaurant.ts
@@ -482,6 +482,130 @@ class Restaurant implements IRestaurant {
     }
 }
 
-// document.addEventListener('DOMContentLoaded', event => {
-//     new Restaurant('.app', restPositions, restDepartments);
-// });
+const restPositions: IPositionsId = {
+    1: 'manager',
+    2: 'barman',
+    3: 'cook',
+    4: 'waiter',
+};
+
+const restDepartments: IDepartment[] = [
+    {
+        title: 'Bar',
+        id: '1',
+        employees: [
+            {
+                name: 'James',
+                surname: 'James',
+                departmentId: '1',
+                position: restPositions[1],
+                salary: 5000,
+                isFired: false,
+            },
+            {
+                name: 'Robert',
+                surname: 'Robert',
+                departmentId: '1',
+                position: restPositions[2],
+                salary: 3000,
+                isFired: false,
+            },
+            {
+                name: 'John',
+                surname: 'John',
+                departmentId: '1',
+                position: restPositions[2],
+                salary: 3000,
+                isFired: false,
+            },
+        ],
+    },
+    {
+        title: 'Cook',
+        id: '2',
+        employees: [
+            {
+                name: 'Mary',
+                surname: 'Mary',
+                departmentId: '2',
+                position: restPositions[1],
+                salary: 6000,
+                isFired: false,
+            },
+            {
+                name: 'Patricia',
+                surname: 'Patricia',
+                departmentId: '2',
+                position: restPositions[3],
+                salary: 4000,
+                isFired: false,
+            },
+            {
+                name: 'Jennifer',
+                surname: 'Jennifer',
+                departmentId: '2',
+                position: restPositions[3],
+                salary: 4000,
+                isFired: false,
+            },
+            {
+                name: 'Jennifer',
+                surname: 'Jennifer',
+                departmentId: '2',
+                position: restPositions[3],
+                salary: 4000,
+                isFired: false,
+            },
+        ],
+    },
+    {
+        title: 'Hall',
+        id: '3',
+        employees: [
+            {
+                name: 'Michael',
+                surname: 'Michael',
+                departmentId: '2',
+                position: restPositions[1],
+                salary: 3000,
+                isFired: false,
+            },
+            {
+                name: 'Linda',
+                surname: 'Linda',
+                departmentId: '2',
+                position: restPositions[3],
+                salary: 1000,
+                isFired: false,
+            },
+            {
+                name: 'Elizabeth',
+                surname: 'Elizabeth',
+                departmentId: '2',
+                position: restPositions[3],
+                salary: 1000,
+                isFired: false,
+            },
+            {
+                name: 'Elizabeth',
+                surname: 'Elizabeth',
+                departmentId: '2',
+                position: restPositions[3],
+                salary: 1000,
+                isFired: false,
+            },
+            {
+                name: 'Elizabeth',
+                surname: 'Elizabeth',
+                departmentId: '2',
+                position: restPositions[3],
+                salary: 1000,
+                isFired: true,
+            },
+        ],
+    },
+];
+
+document.addEventListener('DOMContentLoaded', event => {
+    new Restaurant('.app', restPositions, restDepartments);
+});
