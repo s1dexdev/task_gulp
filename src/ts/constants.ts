42f44741f7a744e3e1ddad1466eef081e9d2e8f4
s1dexdev <maksym.se@gmail.com>
2021-12-06 12:23:03 +0200

    added fixes

---
+++ src/ts/constants.ts
@@ -0,0 +1,29 @@
+const path = {
+    build: {
+        html: './build/',
+        js: './build/js/',
+        css: './build/css/',
+    },
+    src: {
+        html: './src/**/*.html',
+        ts: './src/ts/*.ts',
+        scss: './src/scss/*.scss',
+    },
+    watch: {
+        html: './src/**/*.html',
+        ts: './src/ts/*.ts',
+        scss: './src/scss/*.scss',
+    },
+    clean: './build',
+};
+
+const file = {
+    name: {
+        styles: 'styles.css',
+    },
+    suffix: '.min',
+};
+
+const outputStyle = 'compressed';
+
+module.exports = { path, file, outputStyle };
